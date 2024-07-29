4472a17e2c189fd583d182df07b6f403f6603965
noopur patel <noopurpatel003@gmail.com>
2024-07-29 03:39:15 -0400

    Add files via upload

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,98 @@
+﻿using Module4;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Module4
+{
+	public class MainWindowViewModel : BaseViewModel
+	{
+		MovieLogic movieLogic = new MovieLogic();
+
+		private ObservableCollection<Movie> _MovieCollection;
+
+		public ObservableCollection<Movie> MovieCollection
+		{
+			get { return _MovieCollection; }
+			set 
+			{ 
+				_MovieCollection = value;
+				OnPropertyRaised("MovieCollection");
+			}
+		}
+
+		private Movie _SelectedMovie;
+
+		public Movie SelectedMovie
+		{
+			get { return _SelectedMovie; }
+			set 
+			{ 
+				_SelectedMovie = value;
+				OnPropertyRaised("SelectedMovie");
+			}
+		}
+
+        public ICommand AddCommand { get; set; }
+		public ICommand UpdateCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+			AddCommand = new RelayCommand(AddCommandHandler, CanAddCommand);
+			UpdateCommand = new RelayCommand(UpdateCommandHandler, CanUpdateCommand);
+			DeleteCommand = new RelayCommand(DeleteCommandHandler, CanDeleteCommand);
+			movieLogic.AddMovie(new Movie(1, "The Shawshank Redemption", Genre.Drama, 1994, "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", 142, 9.3, "Frank Darabont", new ObservableCollection<string> { "Tim Robbins", "Morgan Freeman", "Bob Gunton" }, true));
+			movieLogic.AddMovie(new Movie(2, "The Godfather", Genre.Drama, 1972, "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", 175, 9.2, "Francis Ford Coppola", new ObservableCollection<string> { "Marlon Brando", "Al Pacino", "James Caan" }, true));
+			movieLogic.AddMovie(new Movie(3, "The Dark Knight", Genre.Action, 2008, "When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.", 152, 9.0, "Christopher Nolan", new ObservableCollection<string> { "Christian Bale", "Heath Ledger", "Aaron Eckhart" }, true));
+			MovieCollection = movieLogic.moviesCollection;
+		}
+
+		private void DeleteCommandHandler(object obj)
+		{
+			if(SelectedMovie !=null)
+				MovieCollection.Remove(SelectedMovie);
+		}
+
+		private bool CanDeleteCommand(object obj)
+		{
+			return true;
+		}
+
+		private void AddCommandHandler(object obj)
+		{
+			AddUpdateMovieDialog addUpdateMovieDialog = new AddUpdateMovieDialog(SelectedMovie);
+			var dailog = addUpdateMovieDialog.ShowDialog();
+			var vm = addUpdateMovieDialog.DataContext as AddUpdateMovieViewModel;
+			MovieCollection.Add(vm.AddedMovie);
+		}
+
+		private bool CanAddCommand(object obj)
+		{
+			return true;
+		}
+
+
+		private void UpdateCommandHandler(object obj)
+		{
+			AddUpdateMovieDialog addUpdateMovieDialog = new AddUpdateMovieDialog(SelectedMovie);
+			var dailog = addUpdateMovieDialog.ShowDialog();
+			var vm = addUpdateMovieDialog.DataContext as AddUpdateMovieViewModel;
+			MovieCollection.Remove(SelectedMovie);
+			MovieCollection.Add(vm.AddedMovie);
+		}
+
+		private bool CanUpdateCommand(object obj)
+		{
+			return true;
+		}
+	}
+}
---
+++ MovieLogic.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Module4
+{
+	// Movie Management System class
+	public class MovieLogic : IMovie
+	{
+		public ObservableCollection<Movie> moviesCollection; // Using linked list to store movies
+		private Stack<string> actionHistory; // Using stack to maintain action history
+		private Dictionary<int, Movie> movieDictionary; // Using dictionary for fast movie lookup by ID
+
+		// Constructor
+		public MovieLogic()
+		{
+			moviesCollection = new ObservableCollection<Movie>();
+			actionHistory = new Stack<string>();
+			movieDictionary = new Dictionary<int, Movie>();
+		}
+
+		// Method to add a movie
+		public void AddMovie(Movie movie)
+		{
+			moviesCollection.Add(movie);
+			actionHistory.Push($"Added movie: {movie.Title}");
+			movieDictionary.Add(movie.MovieID, movie);
+		}
+
+		// Method to view all movies
+		public void ViewAllMovies()
+		{
+			foreach (var movie in moviesCollection)
+			{
+				movie.DisplayDetails();
+				Console.WriteLine();
+			}
+		}
+
+		// Method to update a movie
+		public void UpdateMovie(Movie movie)
+		{
+			Movie movieToUpdate = new Movie(); 
+			if (movie!=null)
+			{
+				movieToUpdate.Title = movie.Title;
+				movieToUpdate.Genre = movie.Genre;
+				movieToUpdate.ReleaseYear = movie.ReleaseYear;
+				movieToUpdate.Description = movie.Description;
+				movieToUpdate.Duration = movie.Duration;
+				movieToUpdate.Rating = movie.Rating;
+				movieToUpdate.Director = movie.Director;
+				movieToUpdate.Cast = movie.Cast;
+				movieToUpdate.IsAvailable = movie.IsAvailable;
+				actionHistory.Push($"Updated movie: {movie.Title}");
+			}
+			else
+			{
+				Console.WriteLine("Movie not found.");
+			}
+		}
+
+		// Method to delete a movie
+		public void DeleteMovie(int movieID)
+		{
+			Movie movieToDelete;
+			if (movieDictionary.TryGetValue(movieID, out movieToDelete))
+			{
+				moviesCollection.Remove(movieToDelete);
+				actionHistory.Push($"Deleted movie: {movieToDelete.Title}");
+				movieDictionary.Remove(movieID);
+				Console.WriteLine("Movie deleted successfully.");
+			}
+			else
+			{
+				Console.WriteLine("Movie not found.");
+			}
+		}
+
+		// Method to search for a movie by title
+		public void SearchMovieByTitle(string title)
+		{
+			Movie movie = moviesCollection.FirstOrDefault(m => m.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+			if (movie != null)
+			{
+				movie.DisplayDetails();
+			}
+			else
+			{
+				Console.WriteLine("Movie not found.");
+			}
+		}
+
+		// Method to search for movies by genre
+		public void SearchMoviesByGenre(Genre genre)
+		{
+			var matchingMovies = moviesCollection.Where(m => m.Genre == genre);
+			if (matchingMovies != null && matchingMovies.Any())
+			{
+				foreach (var movie in matchingMovies)
+				{
+					movie.DisplayDetails();
+					Console.WriteLine();
+				}
+			}
+			else
+			{
+				Console.WriteLine("No movies found for the specified genre.");
+			}
+		}
+
+		// Method to display action history
+		public void DisplayActionHistory()
+		{
+			Console.WriteLine("Action History:");
+			foreach (var action in actionHistory)
+			{
+				Console.WriteLine(action);
+			}
+		}
+	}
+}
